6e32ef3f6f1b3a3461c81b2d930ee3f7ad2ce283
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-02-27 02:28:00 +0100

    Fix hash routing issues

--- src/js/app.ts
+++ src/js/app.ts
@@ -7,21 +7,29 @@ import Layout from "./components/Layout";
 require('materialize-css');
 
 let isFirstMatch = true;
+let lastPath = null;
 
 function createResolver(component, props = {}) {
   return {
     onmatch(args, requestedPath) {
-      // On n'effectue pas le tracking de la première url ici
-      // Le code analytics directement dans la page s'en charge
-      // @ts-ignore
-      if (!isFirstMatch && window._paq) {
+      const path = requestedPath.split('#')[0];
+
+      // The lastPath check is necessary because Mithril reacts to every change of hash,
+      // but we are only interested in change of path without the hash
+      if (!isFirstMatch && path !== lastPath) {
+        // On n'effectue pas le tracking de la première url ici
+        // Le code analytics directement dans la page s'en charge
         // @ts-ignore
-        window._paq.push(['setCustomUrl', requestedPath]);
-        // @ts-ignore
-        window._paq.push(['trackPageView']);
+        if (window._paq) {
+          // @ts-ignore
+          window._paq.push(['setCustomUrl', requestedPath]);
+          // @ts-ignore
+          window._paq.push(['trackPageView']);
+        }
       }
 
       isFirstMatch = false;
+      lastPath = path;
     },
     render: () => {
       return m(Layout, m(component, props));
@@ -47,7 +55,10 @@ const routes = {
 };
 
 years.forEach(year => {
-  routes['/' + year.data.meta.year] = createResolver(StandaloneScoreboard, year);
+  routes['/' + year.data.meta.year] = createResolver(StandaloneScoreboard, {
+    key: year.data.meta.year, // Prevent Mithril re-using a scoreboard and its scorer/hasher
+    ...year,
+  });
 });
 
 m.route(root, '/', routes);
--- src/js/components/StandaloneScoreboard.ts
+++ src/js/components/StandaloneScoreboard.ts
@@ -8,6 +8,7 @@ const EMPTY_HASH = /^#0+$/;
 export default class StandaloneScoreboard implements m.Component {
   hashReader: NumericHashReader;
   missions: MissionObject;
+  lastMissions: string;
 
   oninit(vnode) {
     const scorer = vnode.attrs.scorer as AbstractScorer<MissionObject, any>;
@@ -35,22 +36,28 @@ export default class StandaloneScoreboard implements m.Component {
         console.warn(e);
       }
     }
+
+    this.lastMissions = JSON.stringify(this.missions);
   }
 
   view(vnode) {
     const scorer = vnode.attrs.scorer as AbstractScorer<MissionObject, any>;
     const data = vnode.attrs.data as Year;
-    let missionHash = '#' + this.hashReader.encode(vnode.state.missions);
+
+    const missionsJson = JSON.stringify(this.missions);
+
+    let missionHash = '#' + this.hashReader.encode(this.missions);
 
     if (EMPTY_HASH.test(missionHash)) {
       missionHash = '';
-      if (window.location.hash) {
-        window.location.hash = '';
-      }
-    } else {
-      if (missionHash !== window.location.hash) {
-        window.location.hash = missionHash;
-      }
+    }
+
+    // We need to check lastMissions because we only want to update the url when the data actually changed
+    // If we compared with the current location hash, it would try updating the hash when we're navigating away from the page to a page that doesn't have a hash
+    if (missionsJson !== this.lastMissions) {
+      window.location.hash = missionHash;
+
+      this.lastMissions = missionsJson;
     }
 
     return m(Scoreboard, {
